909bd692806298aac398204d28497fd2d1af5064
Anant Gaire <56.anant@gmail.com>
2021-03-18 07:10:55 +0545

    middleware

--- server/app.js
+++ server/app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
 
+//middelware
+
+const middleware = (req, res, next) => {
+  console.log(`hello my middleware`);
+  next();
+};
+
 app.get('/', (req, res) => {
   res.send('Hello from server');
 });
-app.get('/about', (req, res) => {
+app.get('/about', middleware, (req, res) => {
+  console.log(`hello my about`);
   res.send('Hello from server, this is about page');
 });
 app.get('/contact', (req, res) => {
