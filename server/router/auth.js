31d20ba1db93c56948d8369ead785f592e2fee8b
Anant Gaire <56.anant@gmail.com>
2021-03-20 07:22:06 +0545

    Password Hashing using Bcrypt.JS

--- server/router/auth.js
+++ server/router/auth.js
@@ -50,13 +50,15 @@ router.post('/register', async (req, res) => {
 
     if (userExist) {
       return res.status(422).json({ error: 'email already exist' });
+    } else if (password != cpassword) {
+      return res.status(422).json({ error: 'password not matched' });
+    } else {
+      const user = new User({ name, email, phone, work, password, cpassword });
+
+      await user.save();
+
+      res.status(201).json({ message: 'User register successfuly' });
     }
-
-    const user = new User({ name, email, phone, work, password, cpassword });
-
-    await user.save();
-
-    res.status(201).json({ message: 'User register successfuly' });
   } catch (err) {
     console.log(error);
   }
