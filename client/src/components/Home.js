50ffb79fc5b27ca4eaa221d2a4e3bf6b04aa3fc5
Anant Gaire <56.anant@gmail.com>
2021-05-17 10:08:28 +0545

    home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div>
-      <p className="pt-5">Welcome</p>
-      <h1>We are mern developers</h1>
-    </div>
+    <>
+      <div className="home-page">
+        <div className="home-div">
+          <p className="pt-5">Welcome</p>
+          <h1>We are mern developers</h1>
+        </div>
+      </div>
+    </>
   );
 };
 
